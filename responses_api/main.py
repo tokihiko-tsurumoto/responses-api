e1247758546ebd86eea199a55be4da284ee12e8c
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-03-20 13:03:00 +0900

    feat:I varidated other feature of the Responses API.

--- responses_api/main.py
+++ responses_api/main.py
@@ -4,9 +4,21 @@ from openai import OpenAI
 
 client = OpenAI()
 
-response = client.responses.create(
+# Create a response
+create_response = client.responses.create(
     model="gpt-4o-mini",
     input="ユニコーンに関する寝かしつけの物語を3文で話してください。",
 )
+id = create_response.id
+print(create_response)
 
-print(response)
+# List input items
+input_items_response = client.responses.input_items.list(id)
+print(input_items_response)
+
+# Retrieve a response by id
+get_response = client.responses.retrieve(id)
+print(get_response)
+
+# Delete a response by id
+client.responses.delete(id)
