da2eba7f3c7786afe96381b6195f9e47385fc6c8
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-03-21 07:57:39 +0900

    feat:varidate simple text input

--- responses_api/text_generation_and_prompting.py
+++ responses_api/text_generation_and_prompting.py
@@ -5,9 +5,18 @@ from openai import OpenAI
 client = OpenAI()
 
 response = client.responses.create(
-    model="gpt-4o",
-    instructions="Talk like a pirate.",
+    model="gpt-4o-mini",
+    instructions="Talk like a pirate.",  # only applies to the current response generation request
     input="Are semicolons optional in JavaScript?",
 )
 
-print(response)
+# developer message enable to persist the same model instructions across the turn
+# response = client.responses.create(
+#     model="gpt-4o-mini",
+#     input=[
+#         {"role": "developer", "content": "Talk like a pirate."},
+#         {"role": "user", "content": "Are semicolons optional in JavaScript?"},
+#     ],
+# )
+
+print(response.output_text)
