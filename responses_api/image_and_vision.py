c25c8436d10f441da8e189b3388a662e280f5d6a
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-03-21 08:03:27 +0900

    feat:varidate image input

--- responses_api/image_and_vision.py
+++ responses_api/image_and_vision.py
@@ -1,2 +1,30 @@
 # https://platform.openai.com/docs/guides/images?api-mode=responses
 
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_text",
+                    "text": "これらの画像には何が含まれていますか? 違いはありますか?",
+                },
+                {
+                    "type": "input_image",
+                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+                {
+                    "type": "input_image",
+                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+            ],
+        }
+    ],
+)
+
+print(response.output_text)
--- responses_api/text_generation_and_prompting.py
+++ responses_api/text_generation_and_prompting.py
@@ -6,16 +6,16 @@ client = OpenAI()
 
 response = client.responses.create(
     model="gpt-4o-mini",
-    instructions="Talk like a pirate.",  # only applies to the current response generation request
-    input="Are semicolons optional in JavaScript?",
+    instructions="海賊のように話しなさい。",  # only applies to the current response generation request
+    input="JavaScript ではセミコロンはオプションですか？",
 )
 
 # developer message enable to persist the same model instructions across the turn
 # response = client.responses.create(
 #     model="gpt-4o-mini",
 #     input=[
-#         {"role": "developer", "content": "Talk like a pirate."},
-#         {"role": "user", "content": "Are semicolons optional in JavaScript?"},
+#         {"role": "developer", "content": "海賊のように話しなさい。"},
+#         {"role": "user", "content": "JavaScript ではセミコロンはオプションですか？"},
 #     ],
 # )
 
